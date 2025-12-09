fd4b3d5d0f632a6f0db04f193c16748788cb50db
Leo Paul Larkin <lplinkl00@gmail.com>
2025-12-09 12:43:06 +0800

    fixing analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { injectSpeedInsights } from '@vercel/speed-insights'
+import { Analytics } from '@vercel/analytics/react'
 import './index.css'
 import App from './App'
 
@@ -9,6 +10,7 @@ injectSpeedInsights()
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
 
